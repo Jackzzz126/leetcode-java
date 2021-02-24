d4726b4c3ed49ae9fbc1f128590535b63d983b60
zhengzhe17 <zhengzhe17@jd.com>
2021-02-24 15:32:41 +0800

    udpate

--- src/main/java/org/jack/SpringBootTest/AlgTest.java
+++ src/main/java/org/jack/SpringBootTest/AlgTest.java
@@ -23,44 +23,6 @@ public class AlgTest {
         //List<Integer> numList = ListNode.list2Arr(head);
         //log.info(numList.toString());
 
-        int[] arr = {1, 5, 11, 5};
-        boolean result = this.canPartition(arr);
-        log.info("{}", result);
-
         log.info("alg test end running...");
     }
-    public boolean canPartition(int[] nums) {
-        if(nums.length < 2) {
-            return false;
-        }
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-        }
-        if(sum % 2 != 1) {
-            return false;
-        }
-        int target = sum / 2;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] > target) {
-                return false;
-            }
-        }
-
-        boolean[][] dp = new boolean[nums.length + 1][target + 1];
-        for (int i = 0; i < (target + 1); i++) {
-            dp[0][i] = false;
-        }
-        for (int i = 0; i < (nums.length + 1); i++) {
-            dp[i][0] = true;
-        }
-        for (int i = 1; i < (nums.length +1) ; i++) {
-            for (int j = 1; j < (target + 1) ; j++) {
-                dp[i][j] = dp[i - 1][j] | dp[i - 1][j-nums[i-1]];
-            }
-        }
-
-        return dp[nums.length][target];
-    }
 }
-
