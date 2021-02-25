095af580dff746131f899e14876f4d7581d96365
zhengzhe17 <zhengzhe17@jd.com>
2021-02-25 15:14:29 +0800

    update 450

--- problem-files/450.删除二叉搜索树中的节点.java
+++ problem-files/450.删除二叉搜索树中的节点.java
@@ -22,7 +22,41 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        
+        if(root == null) {
+            return null;
+        }
+
+        if(key < root.val) {
+            root.left = this.deleteNode(root.left, key);
+        } else if(root.val == key) {
+            if(root.right == null) {
+                if(root.left == null) {
+                    return null;
+                } else {
+                    return root.left;
+                }
+            } else {
+                if(root.left == null) {
+                    return root.right;
+                } else {
+                    root.val = getMinVal(root.right);
+                    root.right = this.deleteNode(root.right, root.val);
+                }
+            }
+        } else {
+            root.right = this.deleteNode(root.right, key);
+        }
+        return root;
+    }
+    private int getMinVal(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.left == null) {
+            return root.val;
+        } else {
+            return this.getMinVal(root.left);
+        }
     }
 }
 // @lc code=end
