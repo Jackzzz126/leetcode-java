76ea032b9e3b29b0d3c735265b9570d40d3c1662
zhengzhe17 <zhengzhe17@jd.com>
2021-06-02 11:59:23 +0800

    update to yml

--- src/main/java/org/jack/LeetcodeTest/LeetcodeTestApplication.java
+++ src/main/java/org/jack/LeetcodeTest/LeetcodeTestApplication.java
@@ -1,12 +1,13 @@
 package org.jack.LeetcodeTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeetcodeTestApplication {
-
     public static void main(String[] args) {
-        //SpringApplication.run(SpringBootTestApplication.class, args);
+        //SpringApplication.run(LeetcodeTestApplication.class, args);
         AlgTest algTest = new AlgTest();
         algTest.run();
     }
