77404de6398942d8e9c82970fa4579e207ea56dd
zhengzhe17 <zhengzhe17@jd.com>
2021-06-03 14:29:11 +0800

    comment test code

--- src/main/java/org/jack/LeetcodeTest/AlgTest.java
+++ src/main/java/org/jack/LeetcodeTest/AlgTest.java
@@ -1,7 +1,6 @@
 package org.jack.LeetcodeTest;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jack.ListNode;
 
 /**
  * AlgTest
@@ -14,17 +13,17 @@ public class AlgTest {
     public void run() {
         log.info("alg test start running...");
 
-        String str;
+        //String str;
 
-        str = "[1,2,2,3,null,null,3,4,null,null,4]";
-        TreeNode root = TreeNode.fromString(str);
-        log.info("{}", str);
-        log.info("{}", root.toString());
+        //str = "[1,2,2,3,null,null,3,4,null,null,4]";
+        //TreeNode root = TreeNode.fromString(str);
+        //log.info("{}", str);
+        //log.info("{}", root.toString());
 
-        str = "[1,2,2,3]";
-        ListNode head = ListNode.fromString(str);
-        log.info("{}", str);
-        log.info("{}", head.toString());
+        //str = "[1,2,2,3]";
+        //ListNode head = ListNode.fromString(str);
+        //log.info("{}", str);
+        //log.info("{}", head.toString());
 
 
         log.info("alg test end running...");
